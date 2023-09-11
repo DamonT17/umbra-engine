58f0f2e2730ff37f4f413c2edf48b8ef353679fd
Damon Tregear <damontregear@gmail.com>
2023-09-10 21:23:41 -0700

    org(CMake): configured CMakeLists.txt & config.h.in to create various run/debug configurations in CLion.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by damon on 9/10/2023.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
---
+++ tests/Vector2Tests.cpp
@@ -0,0 +1,3 @@
+//
+// Created by damon on 9/10/2023.
+//

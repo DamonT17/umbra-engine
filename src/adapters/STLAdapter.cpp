750d512393a1e795b42eecaec2805143f9532392
Damon Tregear <damontregear@gmail.com>
2023-10-22 21:39:36 -0700

    core(memory):created an adapter to interface with STL.

--- src/adapters/STLAdapter.cpp
+++ src/adapters/STLAdapter.cpp
@@ -5,5 +5,13 @@
 #include "STLAdapter.h"
 
 namespace UmbraEngine::Adapters {
+    template<typename T, class Allocator>
+    [[nodiscard]] constexpr T* STLAdapter<T, Allocator>::Allocate(const size_t num) {
+        return allocator.Allocate(num * sizeof(T), alignof(T));
+    }
 
+    template<typename T, class Allocator>
+    constexpr void STLAdapter<T, Allocator>::Free(T* const ptr, [[maybe_unused]] const size_t num) noexcept {
+        allocator.Free(ptr);
+    }
 }
--- src/adapters/STLAdapter.h
+++ src/adapters/STLAdapter.h
@@ -5,49 +5,35 @@
 #ifndef UMBRAENGINE_STLADAPTER_H
 #define UMBRAENGINE_STLADAPTER_H
 
-#include "../core/memory/Allocator.h"
+#include <cstddef>
 
 namespace UmbraEngine::Adapters {
     template<typename T, class Allocator>
     class STLAdapter {
     public:
-        using valueType = T;
+        typedef T value_type;
 
     /**
      * @section Constructors
      */
-
         STLAdapter() = delete;
 
-        explicit STLAdapter(Allocator &allocator) noexcept: allocator(allocator) {}
+        explicit STLAdapter(Allocator& allocator) noexcept: allocator(allocator) {}
 
         template<typename U>
-        STLAdapter(const STLAdapter<U, Allocator> &other) noexcept : allocator(other.allocator) {}
+        explicit STLAdapter(const STLAdapter<U, Allocator>& other) noexcept : allocator(other.allocator) {}
 
     /**
      * @section Methods
      * @subsection Public methods
      */
+        [[nodiscard]] constexpr T* Allocate(size_t num);
 
-        [[nodiscard]] constexpr T *Allocate(const size_t num);
-
-        constexpr void Free(T *const ptr, const size_t num) noexcept;
+        constexpr void Free(T* ptr, [[maybe_unused]] size_t num) noexcept;
 
     private:
-        Allocator &allocator;
+        Allocator& allocator;
     };
-
-    template<typename T>
-    using FreeListAdapter = STLAdapter<T, FreeListAllocator>;
-
-    template<typename T>
-    using LinearAdapter = STLAdapter<T, LinearAllocator>;
-
-    template<typename T>
-    using StackAdapter = STLAdapter<T, StackAllocator>;
-
-    template<typename T>
-    using PoolAdapter = STLAdapter<T, PoolAllocator>;
 }
 
 #endif //UMBRAENGINE_STLADAPTER_H
